aafe331a92338cb3b4d61b3f7821b2dde72f84d6
Dan Wagner <danwagner@ksu.edu>
2017-02-10 16:03:01 +0000

    CL_server toggles servo depending on if X is pressed or not.  Can be modified.

---
+++ CL_server.py
@@ -0,0 +1,119 @@
+  GNU nano 2.2.6                                                                  File: CL_server.py                                                                                                                                          
+
+import asyncio
+import websockets
+
+from logging import Logger
+from contextlib import suppress
+
+import Adafruit_PCA9685 # servo libraries from Adafruit
+
+port = 8055
+logger = Logger(__name__)
+
+# Initialize the servo with default I2C address (0x40)
+servo = Adafruit_PCA9685.PCA9685()
+servo.set_pwm_freq(60)
+
+class CLserver(object):
+    def __init__(self, port):
+        self._active_connections = set()
+        self.port = port
+
+    async def start_server(self):
+        logger.info('server starting up')
+        self.server = await websockets.serve(self.handle_new_connection, '0.0.0.0', self.port)
+
+    async def handle_new_connection(self, ws, path):
+        logger.debug('new connection to server')
+        self._active_connections.add(ws)
+        # print(ws)
+        with suppress(websockets.ConnectionClosed):
+            while True:
+                result = await ws.recv()
+                await self.handle_msg(result)
+        self._active_connections.remove(ws)
+
+    async def handle_msg(self, msg):
+        logger.debug('new message handled')
+        msg = msg.split() # This DOES add latency to the messages (having to parse all 4 buttons rather than a single)
+        if msg[0] == "True":
+            servo.set_pwm(0, 0, 650) # Configuration high for Hirec HS-605MG servo.  CHANGE THIS TO SERVO YOU ARE USING.
+        elif msg[0] == "False":
+            servo.set_pwm(0, 0, 170) # Configuration low for Hirec HS-605MG servo.  CHANGE THIS TO SERVO YOU ARE USING.  
+        await self.send(msg[0])
+
+    async def send(self, msg):
+        logger.debug('sending new message')
+        for ws in self._active_connections:
+            asyncio.ensure_future(ws.send(msg))
+
+server = CLserver(port)
+
+
+asyncio.get_event_loop().run_until_complete(server.start_server())
+
+asyncio.get_event_loop().run_forever()
+
+
+
+  GNU nano 2.2.6                                                                  File: CL_server.py                                                                                                                                          
+
+import asyncio
+import websockets
+
+from logging import Logger
+from contextlib import suppress
+
+import Adafruit_PCA9685 # servo libraries from Adafruit
+
+port = 8055
+logger = Logger(__name__)
+
+# Initialize the servo with default I2C address (0x40)
+servo = Adafruit_PCA9685.PCA9685()
+servo.set_pwm_freq(60)
+
+class CLserver(object):
+    def __init__(self, port):
+        self._active_connections = set()
+        self.port = port
+
+    async def start_server(self):
+        logger.info('server starting up')
+        self.server = await websockets.serve(self.handle_new_connection, '0.0.0.0', self.port)
+
+    async def handle_new_connection(self, ws, path):
+        logger.debug('new connection to server')
+        self._active_connections.add(ws)
+        # print(ws)
+        with suppress(websockets.ConnectionClosed):
+            while True:
+                result = await ws.recv()
+                await self.handle_msg(result)
+        self._active_connections.remove(ws)
+
+    async def handle_msg(self, msg):
+        logger.debug('new message handled')
+        msg = msg.split() # This DOES add latency to the messages (having to parse all 4 buttons rather than a single)
+        if msg[0] == "True":
+            servo.set_pwm(0, 0, 650) # Configuration high for Hirec HS-605MG servo.  CHANGE THIS TO SERVO YOU ARE USING.
+        elif msg[0] == "False":
+            servo.set_pwm(0, 0, 170) # Configuration low for Hirec HS-605MG servo.  CHANGE THIS TO SERVO YOU ARE USING.  
+        await self.send(msg[0])
+
+    async def send(self, msg):
+        logger.debug('sending new message')
+        for ws in self._active_connections:
+            asyncio.ensure_future(ws.send(msg))
+
+server = CLserver(port)
+
+
+asyncio.get_event_loop().run_until_complete(server.start_server())
+
+asyncio.get_event_loop().run_forever()
+
+
+
+
